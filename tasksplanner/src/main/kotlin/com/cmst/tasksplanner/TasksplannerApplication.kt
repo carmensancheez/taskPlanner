c56718f8c52777e61b6da8024b9d0cede0f83e6c
Carmen Sanchez <csanchez@okkralabs.com>
2021-04-26 12:51:59 -0400

    first commit

---
+++ tasksplanner/src/main/kotlin/com/cmst/tasksplanner/TasksplannerApplication.kt
@@ -0,0 +1,11 @@
+package com.cmst.tasksplanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TasksplannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TasksplannerApplication>(*args)
+}
---
+++ tasksplanner/src/test/kotlin/com/cmst/tasksplanner/TasksplannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cmst.tasksplanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TasksplannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
